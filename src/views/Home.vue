101f1b02d4038fa5798a71274d11a887deee0ac2
Brian Ford <brianfordcode@gmail.com>
2022-06-20 21:09:19 -0700

    add v-for to preview btns on homepage, give active class to featured index, add interval to switch picture

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,27 +20,28 @@
       </div>
     </div>
 
-
-
+   
     <div class="picture-container">
-      <div class="buttons">
-        <div class="one" @click="getShownSetupPic(1)">1</div>
-        <div class="two" @click="getShownSetupPic(2)">2</div>
-        <div class="three" @click="getShownSetupPic(3)">3</div>
-        <div class="four" @click="getShownSetupPic(4)">4</div>
-        <div class="five" @click="getShownSetupPic(5)">5</div>
+      <!-- FEATURED PICTURE BUTTONS -->
+      <div class="buttons-wrapper">
+        <div v-for="(link, index) in featuredSetups" :key="link">
+          <div
+            :class="{'preview-btn': true, 'active': picIndex === index ? true : false}"
+            @click="picIndex = index"
+          >
+          {{index + 1}}
+          </div>
+        </div>
       </div>
+      <!-- FEATURED IMAGE -->
       <img
         draggable="false"
-        :src="featuredSetups[setupPic]"
+        :src="featuredSetups[picIndex]"
         alt="picture"
         style="width: 100%;"
       >
 
     </div>
-
-
-
 
 
   </div>
@@ -49,24 +50,29 @@
 
 <script>
 
+
 export default {
   name: 'Home',
   components: {
   },
+
+  mounted: function () {
+    window.setInterval(() => {
+      this.picIndex > this.featuredSetups.length - 2 ? this.picIndex = 0 : this.picIndex++
+    }, 7500)
+  },
   data() {
     return {
-      featuredSetups: ["https://picsum.photos/id/0/1000/1000", "https://picsum.photos/id/119/1000/1000", "https://picsum.photos/id/180/1000/1000", "https://picsum.photos/id/252/1000/1000", "https://picsum.photos/id/366/1000/1000"],
-      setupPic: 0
+      featuredSetups: ["https://picsum.photos/id/0/1000/1000",
+                       "https://picsum.photos/id/119/1000/1000",
+                       "https://picsum.photos/id/180/1000/1000",
+                       "https://picsum.photos/id/252/1000/1000",
+                       "https://picsum.photos/id/366/1000/1000"
+                      ],
+      picIndex: 0,
     }
   },
   methods: {
-    getShownSetupPic(i) {
-      if (i === 1) { this.setupPic = 0 }
-      if (i === 2) { this.setupPic = 1 }
-      if (i === 3) { this.setupPic = 2 }
-      if (i === 4) { this.setupPic = 3 }
-      if (i === 5) { this.setupPic = 4 }
-    },
     getStartedBtn() {
       this.$store.state.user ? this.$router.push(`/setups/${this.$store.state.user.uid}`) : this.$store.dispatch('logIn')
     }
@@ -118,7 +124,7 @@ export default {
   position: relative;
 }
 
-.buttons {
+.buttons-wrapper {
   position: absolute;
   color: white;
   z-index: 100000;
@@ -129,17 +135,18 @@ export default {
   right: 30px;
 }
 
-.buttons > * {
+.preview-btn {
   border: 1px solid white;
   width: 30px;
   height: 30px;
   display: flex;
   justify-content: space-around;
   align-items: center;
-  cursor: pointer
+  cursor: pointer;
+  
 }
 
-.buttons > *:hover {
+.preview-btn:hover, .active {
   background-color: rgba(14, 14, 108,0.75);
   border: 1px solid rgba(14, 14, 108,0.75);
 }
